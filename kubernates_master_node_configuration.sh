08502ad2900b611953a0a760bcd3817e3fd35221
srjbis007 <talk2surajbiswas@gmail.com>
2020-02-26 02:14:21 +0530

    small change

--- kubernates_master_node_configuration.sh
+++ kubernates_master_node_configuration.sh
@@ -26,6 +26,10 @@ echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
 echo "###### NEED TO CONFIGURE THE /etc/hosts FILE AND PUT ENTRY FOR WORKER AND MASTER NODE #######"
 echo "`hostname -i | awk -F ' ' '{ print $2 }'` master" >> /etc/hosts
 
+echo "################## INSTALLING containerd.io-1.2.6 ######"
+yum -y install \
+    https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+
 echo "########### Kubernetes REPO FOR DOWNLOADING NEEDFUL RPMS #################"
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
@@ -51,10 +55,6 @@ yum-config-manager \
 echo "#################### ENABLING THE DOCKER-CE-NIGHTLY ################"
 yum-config-manager --enable docker-ce-nightly
 
-echo "################## INSTALLING containerd.io-1.2.6 ######"
-yum -y install \
-    https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
-
 echo "########### INSTALLING DOCKER ########################"
 yum install -y docker-ce docker-ce-cli
 
@@ -65,19 +65,14 @@ systemctl restart docker && systemctl enable docker
 systemctl  restart kubelet && systemctl enable kubelet
 
 echo "############ STARTING THE KUBEADM PROCESSES ##########"
-echo " KINDLY NOTE THE KUBEADM JOIN TAKEN AS IT IS REQUIRED TO USE IN WORKER NODES"
-kubeadm init
-sleep 16
+echo " KINDLY NOTE THE KUBEADM JOIN TAKEN IN /tmp/kubeadm.log AS IT IS REQUIRED TO USE IN WORKER NODES"
+kubeadm init >> /tmp/kubeadm.log
 
 echo "########### CREATING THE Kubernetes CONFIGURATION DIRECTORIES ##########"
-
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
-echo "############ STARTING THE KUBEADM PROCESSES ##########"
-echo " KINDLY NOTE THE KUBEADM JOIN TAKEN IN /tmp/kubeadm.log AS IT IS REQUIRED TO USE IN WORKER NODES"
-kubeadm init >> /tmp/kubeadm.log
 sleep 16
 
 export kubever=$(kubectl version | base64 | tr -d '\n')
