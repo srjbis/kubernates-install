9ab35d6a7efe05eeb8e27ffa065ebc6049293042
srjbis007 <talk2surajbiswas@gmail.com>
2020-02-26 15:03:55 +0530

    updated the kubernetes.repo

--- kubernates_master_node_configuration.sh
+++ kubernates_master_node_configuration.sh
@@ -31,16 +31,14 @@ sudo -s <<EOF
 	https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
 
 	echo "########### Kubernetes REPO FOR DOWNLOADING NEEDFUL RPMS #################"
-	cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-	[kubernetes]
-	name=Kubernetes
-	baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-	enabled=1
-	gpgcheck=1
-	repo_gpgcheck=1
-	gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-		https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-	EOF
+	echo "[kubernetes]" >> /etc/yum.repos.d/kubernetes.repo
+        echo "	name=Kubernetes" >> /etc/yum.repos.d/kubernetes.repo
+        echo "	baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64" >> /etc/yum.repos.d/kubernetes.repo
+        echo "	enabled=1" >> /etc/yum.repos.d/kubernetes.repo
+        echo "	gpgcheck=1" >> /etc/yum.repos.d/kubernetes.repo
+        echo "	repo_gpgcheck=1" >> /etc/yum.repos.d/kubernetes.repo
+        echo "	gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg" >> /etc/yum.repos.d/kubernetes.repo
+        echo "        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg" >> /etc/yum.repos.d/kubernetes.repo
 
 	echo "############  INSTALLING DOCKER DEPENDENCIES YUM, DEVICE-MAPPER AND LVM2 ###################"				  
 	yum install -y yum-utils \
